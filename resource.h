874125e4799390f406b8e59ccda4f37deebb247c
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2016-10-16 00:02:37 +0000

    iii
    git-svn-id: svn://192.168.0.99/FastGomibako@6244 5ada3c48-002d-0410-850d-99865cc6fd0f

--- resource.h
+++ resource.h
@@ -7,6 +7,7 @@
 #define IDC_EDIT_TARGET                 1000
 #define IDC_COMBO_DELETEMETHOD          1002
 #define IDC_COMBO_PRIORITY              1003
+#define IDC_STATIC_TARGET               1004
 
 // Next default values for new objects
 // 
@@ -14,7 +15,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_CONTROL_VALUE         1005
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
